96b849f880c6d70398da1f9580aea54cdafdf770
mjgandhi2305 <138571862+mjgandhi2305@users.noreply.github.com>
2024-08-07 21:43:36 +0530

    Changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 
 import AboutUsCards from "@/components/AboutUs";
 import Avatar1 from "@/components/Avatar1";
+import { Feature } from "@/components/feature";
 import { Hero } from "@/components/Hero";
+import { Keypoints} from "@/components/Keypoints";
 import LandingPage from "@/components/landing-page";
 
 export default function Home() {
@@ -9,6 +11,8 @@ export default function Home() {
         <>
         <LandingPage/>
         <Hero/>
+        <Feature/>
+        <Keypoints/>
         </>
     )
 }
